f87a39aa8122a87062b71a1c396f123b0dc106bb
Max Carter <maxcartr@gmail.com>
2020-06-23 18:59:52 +0100

    Added a placeholder for the create a client section

---
+++ src/main/kotlin/dev/maxc/ui/view/client/ClientCreationName.kt
@@ -0,0 +1,15 @@
+package dev.maxc.ui.view.client
+
+import javafx.fxml.Initializable
+import java.net.URL
+import java.util.*
+
+
+/**
+ * @author Max Carter
+ * @since 06/06/2020
+ */
+class ClientCreationName : Initializable {
+    override fun initialize(location: URL?, resources: ResourceBundle?) {
+    }
+}
